ac5a6581ff79e7a5ecfa6dc5d174dfc961bd8c8f
Alexander Hering <dev.hering@gmail.com>
2023-05-25 22:56:16 +0200

    building out further common structure

--- submodules/__init__.py
+++ submodules/__init__.py
@@ -11,7 +11,8 @@ import os
 
 common_submodules = [os.path.abspath(os.path.join(os.path.dirname(__file__), "common_plugin_controller")), 
                      os.path.abspath(os.path.join(os.path.dirname(__file__), "common_flask_frontend")), 
-                     os.path.abspath(os.path.join(os.path.dirname(__file__), "common_data_backend"))]
+                     os.path.abspath(os.path.join(os.path.dirname(__file__), "common_data_controller")), 
+                     os.path.abspath(os.path.join(os.path.dirname(__file__), "common_scheduler"))]
 for folder in common_submodules:
     if folder not in sys.path:
         sys.path.append(folder)
