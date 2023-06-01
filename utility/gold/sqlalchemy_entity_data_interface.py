0ab357fb216c5f3aac56ab42cce822f0e94e7d94
Alexander Hering <dev.hering@gmail.com>
2023-06-01 18:51:40 +0200

    adding streamlined interface for sql_alchemy from Common Data Controller

---
+++ utility/gold/sqlalchemy_entity_data_interface.py
@@ -0,0 +1,298 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                   Utility
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+
+
+import copy
+from sqlalchemy import and_, or_, not_
+from typing import Optional, Any, List, Union
+from ..bronze import sqlalchemy_utility
+from .filter_mask import FilterMask
+from ..bronze.dictionary_utility import get_filter_depth
+from ..bronze.comparison_utility import COMPARISON_METHOD_DICTIONARY as CMD
+from .abstract_entity_data_interface import EntityDataInterface, handle_gateways
+
+# Dictionary, mapping filter types of filters to SQLAlchemy-compatible filters
+SQLALCHEMY_FILTER_CONVERTER = {
+    "equals": lambda x, y: x == y,
+    "not_equals": lambda x, y: not_(x == y),
+    "contains": lambda x, y: x.contains(y),
+    "not_contains": lambda x, y: not_(x.contains(y)),
+    "is_contained": lambda x, y: x.in_(y),
+    "not_is_contained": lambda x, y: not_(x.in_(y)),
+    "==": lambda x, y: x == y,
+    "!=": lambda x, y: or_(x != y, and_(x is None, y is not None)),
+    "has": lambda x, y: x.contains(y),
+    "not_has": lambda x, y: not_(x.contains(y)),
+    "in": lambda x, y: x.in_(y),
+    "not_in": lambda x, y: not_(x.in_(y))
+}
+
+# Dictionary, defining table for manual linking
+MANUAL_LINKAGE = {
+    "id": {
+        "type": "int",
+        "primary_key": True,
+        "autoincrement": True,
+        "required": True
+    },
+    "linkage": {
+        "type": "str_180",
+        "required": True,
+        "description": "Linkage type."
+    },
+    "source_type": {
+        "type": "str_180",
+        "required": True,
+        "description": "Source type."
+    },
+    "source_key": {
+        "type": "text",
+        "required": True,
+        "description": "Source key."
+    },
+    "target_type": {
+        "type": "str_180",
+        "required": True,
+        "description": "Target type."
+    },
+    "target_key": {
+        "type": "text",
+        "required": True,
+        "description": "Target key."
+    }
+}
+
+
+class SQLAlchemyEntityInterface(EntityDataInterface):
+    """
+    Class representing SQLAlchemy Entity Interface.
+    """
+
+    def __init__(self, environment_profile: dict, entity_profiles: dict, linkage_profiles: dict,
+                 view_profiles: dict = None) -> None:
+        """
+        Initiation method for SQLAlchemy Entity Interface.
+        :param environment_profile: Environment profile.
+        :param entity_profiles: Entity profiles under entity name.
+        :param linkage_profiles: Physical Data Model linkage for connecting to other data entities.
+        :param view_profiles: Visual representation profiles.
+        """
+        super().__init__(environment_profile, entity_profiles, linkage_profiles, view_profiles)
+        self.engine = sqlalchemy_utility.get_engine(environment_profile["arguments"]["database"],
+                                                    encoding=environment_profile["arguments"].get("encoding", "utf-8"))
+        self.base = sqlalchemy_utility.get_automapped_base(self.engine)
+        self.model = sqlalchemy_utility.get_classes_from_base(self.base)
+        self.session_factory = None
+
+    """
+    Initiation methods
+    """
+
+    def initiate_infrastructure(self) -> None:
+        """
+        Method for initiating infrastructure.
+        """
+        # add profile for manual linking
+        global MANUAL_LINKAGE
+        self._entity_profiles["MANUAL_LINKAGE"] = MANUAL_LINKAGE
+
+        # add dataclasses, based off of with schema args enriched profiles, to model
+        for profile in [p for p in self._entity_profiles if p not in self.model]:
+            self._create_dataclass(profile)
+
+        # create infrastructure and define session factory
+        self.base.metadata.create_all(self.engine)
+        self.session_factory = sqlalchemy_utility.get_session_factory(
+            self.engine)
+
+    def _create_dataclass(self, entity_type: str) -> None:
+        """
+        Internal method for creating dataclass for the given entity type.
+        :param entity_type: Entity type.
+        """
+        mapping_profile = copy.deepcopy(
+            {key: {"type": self._entity_profiles[entity_type][key]["type"],
+                   "schema_args": {
+                       "primary_key": self._entity_profiles[entity_type][key].get("primary_key", False),
+                       "nullable": not self._entity_profiles[entity_type][key].get("not_null", False),
+                       "comment": self._entity_profiles[entity_type][key].get("description", "")
+            }} for key in
+                self._entity_profiles[entity_type] if
+                key != "#meta"})
+        desc = self._entity_profiles[entity_type].get(
+            "#meta", {}).get("description", False)
+        if desc:
+            mapping_profile["#meta"] = {"description": desc}
+        for key in mapping_profile:
+            if "autoincrement" in mapping_profile[key]:
+                mapping_profile[key]["schema_args"]["autoincrement"] = mapping_profile[key]["autoincrement"]
+            if "unique" in mapping_profile[key]:
+                mapping_profile[key]["schema_args"]["unique"] = mapping_profile[key]["unique"]
+        self.model[entity_type] = sqlalchemy_utility.create_mapping_for_dictionary(self.base, entity_type, mapping_profile,
+                                                                                   self._linkage_profiles)
+
+    """
+    Gateway methods
+    """
+
+    def convert_filters(self, entity_type: str, filters: List[FilterMask]) -> list:
+        """
+        Method for coverting common filters to SQLAlchemy-filter expressions.
+        :param entity_type: Entity type.
+        :param filters: A list of Filtermasks declaring constraints.
+        :return: Filter expressions.
+        """
+        filter_expressions = []
+        for filtermask in filters:
+            filter_expressions.extend([
+                SQLALCHEMY_FILTER_CONVERTER[exp[1]](getattr(self.model[entity_type], exp[0]),
+                                                    exp[2]) for exp in filtermask.expressions])
+        return filter_expressions
+
+    """
+    Interfacing methods
+    """
+
+    # override
+    def _get_obj(self, entity_type: str, filters: List[FilterMask], **kwargs: Optional[Any]) -> Optional[Any]:
+        """
+        Method for acquring entity as object.
+        :param entity_type: Entity type.
+        :param filters: A list of Filtermasks declaring constraints.
+        :param kwargs: Arbitrary keyword arguments.
+            'return_as_dict': Flag declaring, whether return values should be formatted as dictionaries.
+        :return: Target entity.
+        """
+        with self.session_factory() as session:
+            result = session.query(self.model[entity_type]).filter(
+                *self.convert_filters(entity_type, filters)
+            ).first()
+        return result
+
+    # override
+    def _post_obj(self, entity_type: str, entity_data: dict, **kwargs: Optional[Any]) -> Optional[Any]:
+        """
+        Method for adding new entity.
+        :param entity_type: Entity type.
+        :param entity_data: Dictionary containing entity data.
+        :param kwargs: Arbitrary keyword arguments.
+            'return_as_dict': Flag declaring, whether return values should be formatted as dictionaries.
+        :return: Target entity data.
+        """
+        with self.session_factory() as session:
+            result = self.model[entity_type](**entity_data)
+            session.add(result)
+            session.commit()
+            session.refresh(result)
+        return result
+
+    # override
+    def _patch_obj(self, entity_type: str, filters: List[FilterMask], entity_data: dict, **kwargs: Optional[Any]) -> Optional[Any]:
+        """
+        Method for patching existing entity.
+        :param entity_type: Entity type.
+        :param filters: A list of Filtermasks declaring constraints.
+        :param entity_data: Dictionary containing entity data.
+        :param kwargs: Arbitrary keyword arguments.
+            'return_as_dict': Flag declaring, whether return values should be formatted as dictionaries.
+        :return: Target entity data.
+        """
+        with self.session_factory() as session:
+            result = session.query(self.model[entity_type]).filter(
+                *self.convert_filters(entity_type, filters)
+            ).first()
+            if result:
+                for arg in entity_data:
+                    setattr(result, arg, entity_data[arg])
+                session.commit()
+                session.refresh(result)
+            return result
+
+    # override
+    def _delete_obj(self, entity_type: str, filters: List[FilterMask], **kwargs: Optional[Any]) -> Optional[Any]:
+        """
+        Method for deleting entity.
+        :param entity_type: Entity type.
+        :param filters: A list of Filtermasks declaring constraints.
+        :param kwargs: Arbitrary keyword arguments.
+            'return_as_dict': Flag declaring, whether return values should be formatted as dictionaries.
+        :return: Target entity data.
+        """
+        with self.session_factory() as session:
+            result = session.query(self.model[entity_type]).filter(
+                *self.convert_filters(entity_type, filters)
+            ).first()
+            if result:
+                session.delete(result)
+                session.commit()
+        return result
+
+    # override
+    @handle_gateways(filter_index=2, data_index=None)
+    def get_batch(self, entity_type: str, filters_list: List[List[FilterMask]], **kwargs: Optional[Any]) -> List[dict]:
+        """
+        Method for acquring entity_data for multiple entities.
+        :param entity_type: Entity type.
+        :param filters_list: A list of lists of Filtermasks declaring constraints. Each separate list of Filtermasks describes 'OR'-constraints for one entry.
+        :param kwargs: Arbitrary keyword arguments.
+            'return_as_dict': Flag declaring, whether return values should be formatted as dictionaries.
+        :return: Target entity data.
+        """
+        with self.session_factory() as session:
+            result = session.query(self.model[entity_type]).filter(or_(and_(
+                *self.convert_filters(entity_type, filters)) for filters in filters_list)).all()
+        return result
+
+    # override
+    @handle_gateways(filter_index=None, data_index=2)
+    def post_batch(self, entity_type: str, entity_data: List[dict], **kwargs: Optional[Any]) -> List[dict]:
+        """
+        Method for adding multiple entities.
+        :param entity_type: Entity type.
+        :param entity_data: List of dictionaries containing entity data.
+        :param kwargs: Arbitrary keyword arguments.
+            'return_as_dict': Flag declaring, whether return values should be formatted as dictionaries.
+        :return: Target entity data.
+        """
+        with self.session_factory() as session:
+            result = session.bulk_insert_mappings(
+                self.model[entity_type], entity_data)
+            session.commit()
+        return result
+
+    """
+    Linkage methods
+    """
+
+    # override
+    def get_linked_entities(self, linkage: str, source_filters: List[FilterMask], target_filters: List[FilterMask] = [],
+                            **kwargs: Optional[Any]) -> list:
+        """
+        Method for getting linked entities.
+        :param linkage: Linkage profile.
+        :param source_filters: Source FilterMasks.
+        :param target_filters: Target FilterMasks. Defaults to empty list.
+        :param kwargs: Arbitrary keyword arguments.
+            'return_as_dict': Flag declaring, whether return values should be formatted as dictionaries.
+        :return: Linked entities.
+        """
+        pass
+
+    # override
+    def link_entities(self, linkage: str, source_filters: List[FilterMask], target_filters: List[FilterMask] = [],
+                      **kwargs: Optional[Any]) -> None:
+        """
+        Method for getting linked entities.
+        :param source_filters: Source FilterMasks.
+        :param linkage: Linkage profile.
+        :param target_filters: Target FilterMasks. Defaults to empty list.
+        :param kwargs: Arbitrary keyword arguments.
+            'return_as_dict': Flag declaring, whether return values should be formatted as dictionaries.
+        :return: Linked entities.
+        """
+        pass
