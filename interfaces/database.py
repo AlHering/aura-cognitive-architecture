000c0b17f848b0e393d7a27b70eeb85c68e7d7b4
Alexander Hering <dev.hering@gmail.com>
2023-06-04 17:21:16 +0200

    starting database

--- interfaces/database.py
+++ interfaces/database.py
@@ -9,19 +9,12 @@ from ..configuration import configuration as cfg
 from ..utility.bronze.sqlalchemy_utility import SUPPORTED_DIALECTS, get_engine, get_automapped_base, get_session_factory, get_classes_from_base
 
 
-"""
-Database handles
-"""
-if cfg.ENV["DB_DIALECT"] not in SUPPORTED_DIALECTS:
-    raise NotImplementedError(
-        f"Dialect {cfg.ENV['DB_DIALECT']} not supported by SQLAlchemy Utility!")
-ENGINE = get_engine(cfg.ENV["DB_URL"])
-BASE = get_automapped_base(ENGINE)
-SESSION_FACTORY = get_session_factory(ENGINE)
-MODEL = get_classes_from_base(BASE)
-
-
-"""
-Dataclasses
-"""
-# TODO: Implement dataclasses
+class ACADatabase(object):
+    """
+    Class, representing ACA Database.
+    """
+    def __init__(self) -> None:
+        """
+        Initiation method.
+        """
+        pass
