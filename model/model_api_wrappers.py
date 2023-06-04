7002e37c737bc6ef2394707d15a77b0006a3684a
Alexander Hering <dev.hering@gmail.com>
2023-06-04 19:16:40 +0200

    implemented api wrapper's abstract methods

--- model/model_api_wrappers.py
+++ model/model_api_wrappers.py
@@ -14,4 +14,37 @@ class AbstractAPIWrapper(abc.ABC):
     Abstract class, representing a API wrapper object.
     Such wrappers are used for connecting to model services.
     """
-    pass
+
+    @abc.abstractmethod
+    def check_connection(self, *args: Optional[List], **kwargs: Optional[dict]) -> bool:
+        """
+        Abstract method for checking connection.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
+        :return: True if connection was established successfuly else False.
+        """
+        pass
+
+    @abc.abstractmethod
+    def get_api_url(self, identifier: str, model_id: Any, *args: Optional[List], **kwargs: Optional[dict]) -> str:
+        """
+        Abstract method for acquring API URL for model.
+        :param identifier: Type of identification.
+        :model_id: Identification of specified type.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
+        :return: API URL for given model ID.
+        """
+        pass
+
+    @abc.abstractmethod
+    def collect_metadata(self, identifier: str, model_id: Any, *args: Optional[List], **kwargs: Optional[dict]) -> dict:
+        """
+        Abstract method for acquring model data by identifier.
+        :param identifier: Type of identification.
+        :model_id: Identification of specified type.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
+        :return: Metadata for given model ID.
+        """
+        pass
