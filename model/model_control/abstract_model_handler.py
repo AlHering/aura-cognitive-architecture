fbcbd61578b79ddbf5d9b032262c0899efcd8580
Alexander Hering <dev.hering@gmail.com>
2023-06-04 19:23:05 +0200

    implemented basic abstract model handler

--- model/model_control/abstract_model_handler.py
+++ model/model_control/abstract_model_handler.py
@@ -5,6 +5,10 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+from typing import Any, Optional, List
+import abc
+from abstract_api_wrapper import AbstractAPIWrapper
+from ...utility.bronze import json_utility
 
 
 class AbstractModelHandler(object):
@@ -17,5 +21,64 @@ class AbstractModelHandler(object):
     def __init__(self) -> None:
         """
         Initiation method.
+        """
+        pass
+
+    def import_data(self, import_path: str) -> None:
+        """
+        Method for importing data.
+        :param import_path: Import path.
+        """
+        self.cache = json_utility.load(import_path)
+
+    def export_data(self, export_path: str) -> None:
+        """
+        Method for exporting data.
+        :param export_path: Export path.
+        """
+        json_utility.save(self.cache, export_path)
+
+    @abc.abstractmethod
+    def load_model_folder(self, *args: Optional[List], **kwargs: Optional[dict]) -> None:
+        """
+        Abstract method for loading model folder.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
+        """
+        pass
+
+    @abc.abstractmethod
+    def update_metadata(self, *args: Optional[List], **kwargs: Optional[dict]) -> None:
+        """
+        Abstract method for updating cached metadata.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
+        """
+        pass
+
+    @abc.abstractmethod
+    def organize_models(self, *args: Optional[List], **kwargs: Optional[dict]) -> None:
+        """
+        Abstract method for organizing local models.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
+        """
+        pass
+
+    @abc.abstractmethod
+    def download_model(self, *args: Optional[List], **kwargs: Optional[dict]) -> None:
+        """
+        Abstract method for downloading a model.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
+        """
+        pass
+
+    @abc.abstractmethod
+    def download_asset(self, *args: Optional[List], **kwargs: Optional[dict]) -> None:
+        """
+        Abstract method for downloading an asset.
+        :param args: Arbitrary arguments.
+        :param kwargs: Arbitrary keyword arguments.
         """
         pass
