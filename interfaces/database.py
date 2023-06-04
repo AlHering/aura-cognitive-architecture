4da8eb803ab7df93fc7bd07e4f1604ebe8875d42
Alexander Hering <dev.hering@gmail.com>
2023-06-04 12:45:45 +0200

    starting direct database backend

---
+++ interfaces/database.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           aura-cognitive-architecture
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+from ..configuration import configuration as cfg
+from ..utility.bronze.sqlalchemy_utility import get_engine, get_automapped_base, get_session_factory
