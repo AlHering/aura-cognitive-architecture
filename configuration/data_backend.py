3ed70bda154b540b6bc180abe1ce26e30c33b36e
Alexander Hering <dev.hering@gmail.com>
2023-06-04 16:51:06 +0200

    starting base configuration of backend module

---
+++ configuration/data_backend.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           aura-cognitive-architecture
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+ENTITY_PROFILE = {
+
+}
+
+LINKAGE_PROFILE = {
+
+}
+
+VIEW_PROFILE = {
+
+}
