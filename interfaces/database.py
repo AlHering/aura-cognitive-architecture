bfb881cb2b658ca0973f19a22e8ae20a21c80e4a
Alexander Hering <dev.hering@gmail.com>
2023-06-04 17:39:50 +0200

    starting dynamic database configuration

--- interfaces/database.py
+++ interfaces/database.py
@@ -6,15 +6,21 @@
 ****************************************************
 """
 from ..configuration import configuration as cfg
-from ..utility.bronze.sqlalchemy_utility import SUPPORTED_DIALECTS, get_engine, get_automapped_base, get_session_factory, get_classes_from_base
+from ..utility.gold.sqlalchemy_entity_data_interface import SQLAlchemyEntityInterface as DBInterface
 
 
-class ACADatabase(object):
+class ACADatabase(DBInterface):
     """
     Class, representing ACA Database.
     """
+
     def __init__(self) -> None:
         """
         Initiation method.
         """
-        pass
+        super().__init__(environment_profile={
+
+        },
+            entity_profiles=cfg.ENTITY_PROFILE,
+            linkage_profiles=cfg.LINKAGE_PROFILE,
+            view_profiles=cfg.VIEW_PROFILE)
