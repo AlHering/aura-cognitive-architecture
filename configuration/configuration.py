9c3e5cf9036379318ae25b70dfcf355cff80f48b
Alexander Hering <dev.hering@gmail.com>
2023-06-08 13:45:42 +0200

    renamed entity data interface python file

--- configuration/configuration.py
+++ configuration/configuration.py
@@ -10,9 +10,16 @@ from dotenv import load_dotenv
 import paths as PATHS
 import urls as URLS
 from configuration.database_configuration import ENTITY_PROFILE, LINKAGE_PROFILE, VIEW_PROFILE
+from ..interfaces.database import ACADatabase
 
 
 """
 Environment file
 """
 ENV = load_dotenv(os.path.join(PATHS.PACKAGE_PATH, ".env"))
+
+
+"""
+Shared Database Interface
+"""
+DB = ACADatabase()
