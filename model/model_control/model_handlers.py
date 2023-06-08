a91ca55bf9d0064ef7eeffdb201ba4d22196e23c
Alexander Hering <dev.hering@gmail.com>
2023-06-08 13:55:51 +0200

    restructured abstract handler

--- model/model_control/model_handlers.py
+++ model/model_control/model_handlers.py
@@ -9,6 +9,7 @@ from typing import Any, Optional, List
 import abc
 from ...configuration import configuration as cfg
 import os
+from logging import Logger
 import copy
 from ...utility.bronze import json_utility, hashing_utility, dictionary_utility
 from ...utility.gold.entity_data_interface import EntityDataInterface
@@ -27,22 +28,25 @@ class AbstractModelHandler(object):
         :param db_interface: Entity Data Interface.
         :param api_wrapper_dict: Dictionary, mapping source to API wrapper.
         """
+        self._logger = Logger["ModelHandler"]
         self._db = db_interface
         self._apis = api_wrapper_dict
         self._cache = {}
 
-    def import_data(self, import_path: str) -> None:
+    def import_cache(self, import_path: str) -> None:
         """
-        Method for importing data.
+        Method for importing cache data.
         :param import_path: Import path.
         """
+        self._logger.log(f"Importing cache from '{import_path}'...")
         self._cache = json_utility.load(import_path)
 
-    def export_data(self, export_path: str) -> None:
+    def export_cache(self, export_path: str) -> None:
         """
-        Method for exporting data.
+        Method for exporting cache data.
         :param export_path: Export path.
         """
+        self._logger.log(f"Exporting cache to '{export_path}'...")
         json_utility.save(self._cache, export_path)
 
     @abc.abstractmethod
@@ -94,12 +98,14 @@ class StabeDiffusionModelHandler(AbstractModelHandler):
         :param api_wrapper_dict: Dictionary, mapping source to API wrapper.
         """
         super().__init__(db_interface, api_wrapper_dict)
+        self._logger = Logger["StabeDiffusionModelHandler"]
 
-    def load_model_folder(self, *args: Optional[List], **kwargs: Optional[dict]) -> None:
+    def load_model_folder(self, model_folder: str, ignored_sub_folders: List[str] = []) -> None:
         """
         Method for loading model folder.
-        :param args: Arbitrary arguments.
-        :param kwargs: Arbitrary keyword arguments.
+        :param model_folder: Model folder to load.
+        :param ignored_sub_folders: Subfolder parts to ignore.  
+            Defaults to an empty list.
         """
         pass
 
