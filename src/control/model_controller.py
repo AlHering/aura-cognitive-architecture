eba481fb836f2a5da11a39b80491cdfd03117b73
Alexander Hering <dev.hering@gmail.com>
2023-06-15 21:17:23 +0200

    implemented basic setup with handlers

--- src/control/model_controller.py
+++ src/control/model_controller.py
@@ -5,6 +5,9 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+from typing import List, Any
+from src.model.model_control.model_handlers import StabeDiffusionModelHandler
+from src.interfaces.model_database import ModelDatabase
 
 
 class ModelController(object):
@@ -14,11 +17,31 @@ class ModelController(object):
     model services for collecting metadata and downloading assets.
     """
 
-    def __init__(self) -> None:
+    def __init__(self, handlers: List[dict] = None) -> None:
         """
         Initiation method.
+        :param handlers: List of handlers as dictionaries of the form
+            {
+                "handler_type": <Handler Type>,
+                "db_interface": <ModelDatabase>,
+                "api_wrapper_dict": <API-Wrapper-dict>,
+                "cache": <Cache>
+            }
+            Defaults to None in which case the controller awaits new handlers to be added.
         """
-        pass
+        self.handler_classes = {
+            "stablediffusion": StabeDiffusionModelHandler
+        }
+        self.handlers = [self.handler_classes[entry["handler_type"]](
+            entry["db_interface"], entry["api_wrapper_dict"], entry.get("cache")) for entry in handlers]
+
+    def add_handler(self, db_interface: ModelDatabase, handler_type: Any, api_wrapper_dict, cache: dict = None) -> None:
+        """
+        Method for adding handler.
+        :param handler_class: Handler class for initiating handler.
+        """
+        self.handlers.append(self.handler_classes[handler_type](
+            db_interface, api_wrapper_dict, cache))
 
     def _integrate_into_frontend() -> None:
         """
