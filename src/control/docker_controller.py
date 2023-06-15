5fc1393f8296044664d36c51094a8e3d49393f2b
Alexander Hering <dev.hering@gmail.com>
2023-06-15 21:39:06 +0200

    added cache handling

--- src/control/docker_controller.py
+++ src/control/docker_controller.py
@@ -5,6 +5,7 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+from src.utility.bronze import json_utility
 
 
 class DockerController(object):
@@ -19,3 +20,19 @@ class DockerController(object):
             Defaults to None in which case an empty cache is created.
         """
         self._cache = cache if cache is not None else {}
+
+    def import_cache(self, import_path: str) -> None:
+        """
+        Method for importing cache data.
+        :param import_path: Import path.
+        """
+        self._logger.log(f"Importing cache from '{import_path}'...")
+        self._cache = json_utility.load(import_path)
+
+    def export_cache(self, export_path: str) -> None:
+        """
+        Method for exporting cache data.
+        :param export_path: Export path.
+        """
+        self._logger.log(f"Exporting cache to '{export_path}'...")
+        json_utility.save(self._cache, export_path)
