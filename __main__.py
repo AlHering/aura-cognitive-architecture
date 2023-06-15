e46b1f56b50cc95252a0db1ada90657eaf701c86
Alexander Hering <dev.hering@gmail.com>
2023-06-15 18:34:27 +0200

    heavily restructured due to submodule removal

--- __main__.py
+++ __main__.py
@@ -5,9 +5,19 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
-from configuration.common_frontend_config import global_config
-from view.flask_frontend.flask_app_controller import CommonFlaskApplicationController
-aura_app_controller = CommonFlaskApplicationController(global_config)
+import sys
+import os
+package_root = os.path.abspath(os.path.dirname(__file__))
+cpc_root = os.path.join(package_root, "submodules", "common_plugin_controller")
+for root in [package_root, cpc_root]:
+    if root not in sys.path:
+        sys.path.append(root)
 
+if __name__ == "__main__":
+    print(sys.path)
+    from configuration.common_frontend_config import global_config
+    from view.flask_frontend.flask_app_controller import CommonFlaskApplicationController
 
-aura_app_controller.run_app(global_config)
+    aura_app_controller = CommonFlaskApplicationController(global_config)
+
+    aura_app_controller.run_app(global_config)
