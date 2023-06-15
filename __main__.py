ecf332c351066350d799212469289dc89798396f
Alexander Hering <dev.hering@gmail.com>
2023-06-15 18:01:41 +0200

    adjusted frontend to new structure

--- __main__.py
+++ __main__.py
@@ -7,4 +7,7 @@
 """
 from configuration.common_frontend_config import global_config
 from submodules.common_flask_frontend.control.common_flask_application_controller import CommonFlaskApplicationController
-aura_app_controller = CommonFlaskApplicationController()
+aura_app_controller = CommonFlaskApplicationController(global_config)
+
+
+aura_app_controller.run_app()
