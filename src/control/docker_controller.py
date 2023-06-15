7017626f4bba530759276961be4cf7742b0c4300
Alexander Hering <dev.hering@gmail.com>
2023-06-15 21:34:40 +0200

    starting docker controller class

--- src/control/docker_controller.py
+++ src/control/docker_controller.py
@@ -5,3 +5,15 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+
+
+class DockerController(object):
+    """
+    Controller for managing Docker-based environments.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initiation method.
+        """
+        pass
