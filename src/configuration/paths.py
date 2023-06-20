e4b7ad902155f25a2ece216210c06ec51de344d4
Alexander Hering <dev.hering@gmail.com>
2023-06-20 19:59:46 +0200

    adjusted paths to new structure

--- src/configuration/paths.py
+++ src/configuration/paths.py
@@ -7,12 +7,14 @@
 """
 import os
 
-
-PACKAGE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SUBMODULE_PATH = os.path.join(PACKAGE_PATH, "submodules")
+PACKAGE_PATH = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
+SOURCE_PATH = os.path.join(PACKAGE_PATH, "src")
+DOCS_PATH = s.path.join(PACKAGE_PATH, "docs")
+SUBMODULE_PATH = os.path.join(SOURCE_PATH, "submodules")
 DATA_PATH = os.path.join(PACKAGE_PATH, "data")
-PLUGIN_PATH = os.path.join(PACKAGE_PATH, "plugins")
+PLUGIN_PATH = os.path.join(SOURCE_PATH, "plugins")
 FLASK_COMMON_STATIC = os.path.join(
-    PACKAGE_PATH, "view", "flask_frontend", "common_static")
+    SOURCE_PATH, "view", "flask_frontend", "common_static")
 FLASK_COMMON_TEMPLATES = os.path.join(
-    PACKAGE_PATH, "view", "flask_frontend", "common_templates")
+    SOURCE_PATH, "view", "flask_frontend", "common_templates")
