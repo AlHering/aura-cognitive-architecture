867802ebca385d25158c3347f7527e313cd4ddc8
Alexander Hering <dev.hering@gmail.com>
2023-06-23 11:47:06 +0200

    starting worker node class

---
+++ src/model/docker_control/worker_node.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           aura-cognitive-architecture
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+
+
+class WorkerNode(object):
+    """
+    Class representing a container-based working node.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initiation method.
+        """
+        pass
