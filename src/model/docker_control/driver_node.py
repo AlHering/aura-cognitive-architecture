6e2edb25061a8d1eb0c5efdc637f6b4c1d8c4e07
Alexander Hering <dev.hering@gmail.com>
2023-06-23 12:17:02 +0200

    starting driver node class

---
+++ src/model/docker_control/driver_node.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*           aura-cognitive-architecture
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+from typing import List
+from src.utility.gold.entity_data_interface import EntityDataInterface
+from .worker_node import WorkerNode
+
+
+class DriverNode(object):
+    """
+    Class representing a container-based driver node.
+    """
+
+    def __init__(self, db_interface: EntityDataInterface, worker_nodes: List[WorkerNode]) -> None:
+        """
+        Initiation method.
+        :param db_interface: Database interface.
+        :param worker_nodes: List of worker nodes to initiate driver node with.
+        """
+        self._db = db_interface
+        self.worker_nodes = worker_nodes
