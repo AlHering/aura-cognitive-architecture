5fcb9be10005853ed1291e45dc066d097d796309
Alexander Hering <dev.hering@gmail.com>
2023-06-23 12:07:06 +0200

    added directories position to profile

--- src/model/docker_control/worker_node.py
+++ src/model/docker_control/worker_node.py
@@ -12,7 +12,7 @@ class WorkerNode(object):
     Class representing a container-based working node.
     """
 
-    def __init__(self, setup_profile: dict, interface_profile: dict) -> None:
+    def __init__(self, setup_profile: dict = None, interface_profile: dict = None) -> None:
         """
         Initiation method.
         :param setup_profile: Setup profile as dictionary, containing
@@ -20,6 +20,7 @@ class WorkerNode(object):
             'environment':  List of tuples of the form (<variable>, <value>)
             'arguments': List of tuples of the form (<variable>, <value>)
             'packages': List of packages to install on image level
+            'directories': List of tuples of directories to import of the form (<source path>, <path inside container>)
             'runtime': Runtime type as string
             'runtime_version': Optional runtime version
             'runtime_packages': Runtime packages to install
