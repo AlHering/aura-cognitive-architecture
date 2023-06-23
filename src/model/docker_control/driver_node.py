abf9fa5174fbc3f43beff957e3675aa4dded3988
Alexander Hering <dev.hering@gmail.com>
2023-06-23 12:26:30 +0200

    added working directory

--- src/model/docker_control/driver_node.py
+++ src/model/docker_control/driver_node.py
@@ -15,14 +15,16 @@ class DriverNode(object):
     Class representing a container-based driver node.
     """
 
-    def __init__(self, db_interface: EntityDataInterface, blueprints: list) -> None:
+    def __init__(self, db_interface: EntityDataInterface, blueprints: list, working_directory: str) -> None:
         """
         Initiation method.
         :param db_interface: Database interface.
         :param blueprints: Blueprints for worker nodes.
+        :param working_directory: Working directory.
         """
         self._db = db_interface
         self.blueprints = blueprints
+        self.working_directory: working_directory
 
     def add_blueprint(self, blueprint: Any) -> None:
         """
